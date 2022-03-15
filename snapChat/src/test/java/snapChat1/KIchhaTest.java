bd11bb681b72a6f845040dfa1bf5ba9b5403bec4
User <User@User-PC>
2022-03-15 16:09:43 +0530

    user1

---
+++ snapChat/src/test/java/snapChat1/KIchhaTest.java
@@ -0,0 +1,17 @@
+package snapChat1;
+
+import org.testng.annotations.Test;
+
+public class KIchhaTest 
+{
+@Test
+public void kIchhaTest()
+{
+	System.out.println("sudeep");
+}
+@Test
+public void sahasaSihmaTest()
+{
+	System.out.println("vishnu");
+}
+}
